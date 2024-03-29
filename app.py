7da9147dfd56b38ca6877233e3c0b39487d432a0
mleila1312 <leila.iksil@gmail.com>
2024-03-29 16:03:55 +0100

    changement chemins

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 @app.route('/LH/<lh_number>')
 def acceuil_lh(lh_number):
     '''Renvoie la page d'acceuil d'une lh, stockée dans templates/lh_number/'''
-    return render_template(f'{lh_number}/home.html')
+    return render_template(f'{lh_number}/articles/home.html')
 
 @app.route('/LH/<lh_number>/<article>')
 def un_article(lh_number,article):
     '''Affiche un article stocké dans templates/lh_number/'''
-    return render_template(f'{lh_number}/{article}.html')
+    return render_template(f'{lh_number}/articles/{article}.html')
 
 
 # Pour accéder à l'acceuil d'une LH: ip/LH/"numéro de la LH genre LH1"
