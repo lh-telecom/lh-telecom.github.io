6a70a3cb84f5626829bb14606a120e9f9df7fa37
Nicolas Besson <nicolas.besson@telecom-paris.fr>
2024-03-29 16:47:26 +0100

    Préparation d'une favicon générale

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/LH/<lh_number>')
 def acceuil_lh(lh_number):
