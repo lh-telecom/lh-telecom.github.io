2a13011395a6d1d3cfcca808641a09b659d6c6f0
rambip <apero1808@gmail.com>
2024-03-31 21:46:17 +0200

    réécriture du generateur html en plus lisible

--- traitement.py
+++ traitement.py
@@ -1,46 +1,23 @@
 from yattag import Doc
 from markdown import markdown
 import os
-#dossier où seront les md
-directory = 'articles/LH1/'
-#liste des articles avec les liens pour y accéder
-articles = [ ("/LH/LH1/"+a.split(".")[0]+".html", a.split(".")[0]) for a in os.listdir(directory) ]
 
-for art in os.listdir(directory):
-    html_generator = Doc()
-
-#Option 1: Markdown en fichier
-# article1 = open("PLANNING.md")
-# HTML = markdown(article1.read())
-#Fin option 1
-
-# #Option 2: Markdown en doctstring
-    article= open(directory+"/"+art, encoding="utf-8")
-    TEXTE_MARKDOWN= markdown(article.read())
-
-    HTML=markdown(str(TEXTE_MARKDOWN))
-    # #Fin option 2
-
-    TEMPLATE_VERSION=1
-
-    TITRE=art.split(".")[0] #art=filename.md
+def header(html_generator, titre):
+    doc, tag, text = html_generator.tagtext()
+    with tag('head'):
+        with tag('meta', charset="utf-8"):
+            pass
+        with tag('title', id='titre'):
+            text(titre)
+        with tag("link", rel="icon", type="image/x-icon", href="/static/favicon_dead.ico"):
+            pass
+        with tag("link", href="/static/styles/LH1/accueil.css", type="text/css", rel="stylesheet"):
+            pass
 
 
-
-    def header(html_generator, titre):
-        doc, tag, text = html_generator.tagtext()
-        with tag('head'):
-            with tag('meta', charset="utf-8"):
-                pass
-            with tag('title', id='titre'):
-                text(titre)
-            with tag("link", rel="icon", type="image/x-icon", href="/static/favicon_dead.ico"):
-                pass
-            with tag("link", href="/static/styles/LH1/accueil.css", type="text/css", rel="stylesheet"):
-                pass
-
-    def navbar(html_generator):
-        doc, tag, text = html_generator.tagtext()
+def navbar(html_generator, articles):
+    doc, tag, text = html_generator.tagtext()
+    with tag("div", klass="large_element_hors_grille"):
         with tag("nav"):
             with tag("ul"):
                 with tag("li", klass="menu"):
@@ -59,21 +36,38 @@ for art in os.listdir(directory):
                         text("Find your LH !")
 
 
+
+def genere_debut_page(html_generator, titre_lh):
+    doc, tag, text = html_generator.tagtext()
+    with tag("div", klass="grille_page"):
+        with tag("div", klass="haut_de_page grille_body"):
+            with tag("a", href="/home.html", id="logo"):
+                with tag("img", src="/static/Logo_LH_Necro.png"):
+                    pass
+            with tag("div", id="text_accueil_LH"):
+                text(titre_lh)
+
+
+def genere_article(n_lh, titre_lh, name, autres_articles):
+    path = f'articles/{n_lh}/{name}.md'
+    article= open(path, encoding="utf-8")
+    TEXTE_MARKDOWN= markdown(article.read())
+    article.close()
+
+
+    HTML=markdown(str(TEXTE_MARKDOWN))
+
+    # TODO: ne pas utiliser le nom de fichier comme titre
+    TITRE=name
+
+    html_generator = Doc()
     doc, tag, text = html_generator.tagtext()
 
     with tag('html'):
-        header(html_generator, "LH1 - LH d'outre-tombe")
+        header(html_generator, titre_lh)
         with tag("body"):
-            with tag("div", klass="grille_page"):
-                with tag("div", klass="haut_de_page grille_body"):
-                    with tag("a", href="/home.html", id="logo"):
-                        with tag("img", src="/static/Logo_LH_Necro.png"):
-                            pass
-                    with tag("div", id="text_accueil_LH"):
-                        text("LH1 - LH d'outre-tombe")
-
-            with tag("div", klass="large_element_hors_grille"):
-                navbar(html_generator)
+            genere_debut_page(html_generator, titre_lh)
+            navbar(html_generator, autres_articles)
 
             with tag("div", klass="grille_page"):
                 with tag("div", klass="corps_de_page grille_body"):
@@ -82,90 +76,55 @@ for art in os.listdir(directory):
                     with tag("div", klass="mot_de_la_redaction"):
                         doc.asis(markdown(str(TEXTE_MARKDOWN)))
 
+
+
     html = doc.getvalue()
 
     # genere le html dans "dist"
-    with open("static/LH1/articles/"+art.split(".")[0]+".html", "w",encoding='utf-8') as file:
+    with open(f"static/LH1/articles/{name}.html", "w",encoding='utf-8') as file:
         file.write(html)
 
 
-html_generator = Doc()
+def genere_edition(n_lh, tire_lh, articles):
+    fichier_mot_redaction = open(f"articles/{n_lh}/README.md", encoding="utf-8")
+    mot_redaction = markdown(fichier_mot_redaction.read())
+    fichier_mot_redaction.close()
 
-#Option 1: Markdown en fichier
-# article1 = open("PLANNING.md")
-# HTML = markdown(article1.read())
-#Fin option 1
-
-# #Option 2: Markdown en doctstring
-article= open("articles/mot.md", encoding="utf-8")
-TEXTE_MARKDOWN= markdown(article.read())
-
-HTML=str(TEXTE_MARKDOWN)
-# #Fin option 2
-
-TEMPLATE_VERSION=1
-
-TITRE="Le mot de la rédaction :" #art=filename.md
-
-
-
-def header(html_generator, titre):
+    html_generator = Doc()
     doc, tag, text = html_generator.tagtext()
-    with tag('head'):
-        with tag('meta', charset="utf-8"):
-            pass
-        with tag('title', id='titre'):
-            text(titre)
-        with tag("link", rel="icon", type="image/x-icon", href="/static/favicon_dead.ico"):
-            pass
-        with tag("link", href="/static/styles/LH1/accueil.css", type="text/css", rel="stylesheet"):
-            pass
 
-def navbar(html_generator):
-    doc, tag, text = html_generator.tagtext()
-    with tag("nav"):
-        with tag("ul"):
-            with tag("li", klass="menu"):
-                with tag("a"):
-                    text("les articles de la semaine")
-                with tag('ul', klass="sous"):
-                    for (url, nom) in articles:
-                        with tag("li"):
-                            with tag("a", href=url):
-                                text(nom)
-            with tag("li"):
-                with tag("a", href="evenements.html"):
-                    text("Les derniers numéros")
-            with tag("li"):
-                with tag("a", href="club.html"):
-                    text("Find your LH !")
+    with tag('html'):
+        header(html_generator, titre_lh)
+        with tag("body"):
+            genere_debut_page(html_generator, titre_lh)
+            navbar(html_generator, articles)
+
+            with tag("div", klass="grille_page"):
+                with tag("div", klass="corps_de_page grille_body"):
+                    with tag("h1"):
+                        text("Le mot de la redaction")
+                    with tag("div", klass="mot_de_la_redaction"):
+                        doc.asis(markdown(str(mot_redaction)))
+
+    html = doc.getvalue()
+
+    # genere le html dans "dist"
+    with open(f"static/{n_lh}/home.html", "w",encoding='utf-8') as file:
+        file.write(html)
 
 
-doc, tag, text = html_generator.tagtext()
+if __name__ == "__main__":
+    for (n_lh, titre_lh) in [("LH1", "LH1 - LH d'outre-tombe")]:
 
-with tag('html'):
-    header(html_generator, "LH1 - LH d'outre-tombe")
-    with tag("body"):
-        with tag("div", klass="grille_page"):
-            with tag("div", klass="haut_de_page grille_body"):
-                with tag("a", href="/home.html", id="logo"):
-                    with tag("img", src="/static/Logo_LH_Necro.png"):
-                        pass
-                with tag("div", id="text_accueil_LH"):
-                    text("LH1 - LH d'outre-tombe")
+        directory = f"articles/{n_lh}"
+        article_names = [a.split(".")[0] for a in os.listdir(directory) if a != "README.md"]
 
-        with tag("div", klass="large_element_hors_grille"):
-            navbar(html_generator)
+        # TODO: ne pas utiliser le nom de fichier comme titre
+        articles = [ (f"/LH/{n_lh}/{name}.html", name) for name in article_names]
 
-        with tag("div", klass="grille_page"):
-            with tag("div", klass="corps_de_page grille_body"):
-                with tag("h1"):
-                    text(TITRE)
-                with tag("div", klass="mot_de_la_redaction"):
-                    doc.asis(markdown(str(TEXTE_MARKDOWN)))
+        genere_edition(n_lh, titre_lh, articles)
 
-html = doc.getvalue()
+        for name in article_names:
+            genere_article(n_lh, titre_lh, name, articles)
 
-# genere le html dans "dist"
-with open("static/LH1/home.html", "w",encoding='utf-8') as file:
-    file.write(html)
+
