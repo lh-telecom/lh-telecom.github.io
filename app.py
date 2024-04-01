0e1c8300f7863f2a7b7dedba32adcef539ddcff2
rambip <apero1808@gmail.com>
2024-04-01 14:43:22 +0200

    simplification de l'app flask

--- app.py
+++ app.py
@@ -9,46 +9,25 @@ def gerer_erreur(e):
     return app.send_static_file('404.html'), 404
 
 @app.route('/')
-@app.route('/home.html')
+@app.route('/index.html')
 def accueil():
-    return app.send_static_file('home.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return app.send_static_file('index.html')
 
 @app.route('/LH/<lh_number>')
 def accueil_lh(lh_number):
     '''Renvoie la page d'accueil d'une lh, stockée dans templates/lh_number/'''
     # on décompose le chemin d'accès en deux : le chemin du HTML à renvoyer, et le chemin complet pour savoir si le fichier existe.
-    path = f'{lh_number}/home.html'
-    full_path = os.path.join(app.root_path, 'static', path)
-    if not os.path.exists(full_path):
-        abort(404)
+    path = f'{lh_number}/index.html'
     return app.send_static_file(path)
-    
 
 @app.route('/LH/<lh_number>/<article>.html')
-def un_article(lh_number,article):
-    '''Affiche un article stocké dans templates/lh_number/'''
-    path = f'{lh_number}/articles/{article}.html'
-    full_path = os.path.join(app.root_path, 'static', path)
-    if not os.path.exists(full_path):
-        abort(404)
-    return app.send_static_file(path)
-
-@app.route('/LH/<lh_number>/<article>/')
 def un_article_htmlless(lh_number,article):
     '''Affiche un article stocké dans templates/lh_number/'''
     path = f'{lh_number}/articles/{article}.html'
-    full_path = os.path.join(app.root_path, 'static', path)
-    if not os.path.exists(full_path):
-        abort(404)
     return app.send_static_file(path)
 
 # Pour accéder à l'accueil d'une LH: ip/LH/"numéro de la LH genre LH1"
 # Pour un article de ce numéro: ip/LH/"numéro de la LH genre LH1"/"nom du fichier d'un article genre article_test"
 
 if __name__ == '__main__':
-    app.run(debug=False,port=8080,host="0.0.0.0") # Lance l'appli
+    app.run(debug=False,port=80,host="0.0.0.0") # Lance l'appli
